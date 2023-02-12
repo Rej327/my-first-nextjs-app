118d3743377ca3de0bc1b6338c53c352c7ea6e1a
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-02-12 15:01:46 +0800

    Fetching data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import Link from "next/link";
+import FormPost from "./Form";
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data: { id: number; title: string }[] = await getPosts();
   return (
     <main className="py-8 px-48">
-      <Link
-        className=" bg-teal-500 text-black text-medium py-2 px-4 rounded-md"
-        href={"/dashboard"}
-      >
-        Go to the dashboard
-      </Link>
+      <FormPost />
+      {data.map((post) => (
+        <h1 className="text-lg py-6">{post.title}</h1>
+      ))}
     </main>
   );
 }
